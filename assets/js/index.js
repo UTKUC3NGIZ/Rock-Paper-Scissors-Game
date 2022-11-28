82eb361644124ba7ab3737b51c569a9ad815f399
Utku Cengiz <cekicengiz01@gmail.com>
2022-11-29 01:43:18 +0300

    Generating html and css structure for other steps

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,10 +16,35 @@ window.onclick = function (event) {
   }
 };
 
-
 // vote
 
-const paper = document.getElementById("paper")
-const scissors = document.getElementById("scissors")
-const rock = document.getElementById("rock")
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const rock = document.getElementById("rock");
+const background = document.getElementsByClassName("game__background")[0];
+const pc = document.getElementById("pc");
+const game__conclusion = document.getElementsByClassName("game__conclusion")[0];
+const picked = document.getElementsByClassName("picked")[0];
 
+function deneme(vote) {
+  scissors.style.display = "none";
+  rock.style.display = "none";
+  paper.style.display = "none";
+  background.style.display = "none";
+
+  vote.style.position = "static";
+  vote.style.display = "block";
+  pc.style.display = "block";
+  game__conclusion.style.display = "flex";
+  picked.style.display = "flex";
+}
+
+paper.addEventListener("click", () => {
+  deneme(paper);
+});
+scissors.addEventListener("click", () => {
+  deneme(scissors);
+});
+rock.addEventListener("click", () => {
+  deneme(rock);
+});
